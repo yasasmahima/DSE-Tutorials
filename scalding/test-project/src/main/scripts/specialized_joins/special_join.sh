74a443b0859482ce4a1e06ed36810dc2c2af4b29
aathifs <aathifs@zone24x7.com>
2018-09-20 10:57:23 +0530

    special_joins scripts

--- scalding/test-project/src/main/scripts/specialized_joins/special_join.sh
+++ scalding/test-project/src/main/scripts/specialized_joins/special_join.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env bash
+
+#parameter passing
+for item in $@; do
+case $item in
+(*=*) eval $item;
+esac
+done
+
+#hadoop fs -rm -r -f $outputPath
+rm -r -f $outputPath
+
+pig -x local -stop_on_failure -param inputPath1=$inputPath1 -param inputPath2=$inputPath2 -param schema1=$schema1 -param schema2=$schema2 -param joinField1=$joinField1 -param joinField2=$joinField2 -param join_method=$join_method -param delimeter=$delimeter -param outputPath=$outputPath $pigScriptPath;
